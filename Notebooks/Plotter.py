5f2846d952700e15f9415df21548248b4ed45cef
a-parida12 <abhijeet.parida@tum.de>
2018-03-16 19:28:18 +0100

    all done, code made beautiful

--- Notebooks/Plotter.py
+++ Notebooks/Plotter.py
@@ -1,62 +1,123 @@
+from matplotlib.pyplot import figure, subplot, plot, title, xlabel, ylabel,legend
+from numpy import arange,exp
 
 def plotter_exp(p_exp,dt_all,tend,labels):
     import matplotlib.pyplot as plt
-    #plt.figure()
+    figure()
 
 
     ##subplot for dt=1/2
     dt=dt_all[0]
-    plt.subplot(2, 2, 1)
-    t = np.arange(0,tend,dt)
-    tip = np.arange(0,tend,dt_all[-1])
-    p = 10/(1+(9*np.exp(-tip)))
-    plt.title("function p(t) v/s t for dt="+str(dt))
-    plt.xlabel("t")
-    plt.ylabel("p(t)")
-    handle1,=plt.plot(tip,p,'b',label='Analytic Soln')
-    handle2,=plt.plot(t,p_exp[dt],'r',label=labels)
-    plt.legend(handles=[handle1,handle2])
+    subplot(2, 2, 1)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 10/(1+(9*exp(-tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
 
     ##subplot for dt=1/4
     dt=dt_all[1]
-    plt.subplot(2, 2, 2)
-    t = np.arange(0,tend,dt)
-    tip = np.arange(0,tend,dt_all[-1])
-    p = 10/(1+(9*np.exp(-tip)))
-    plt.title("function p(t) v/s t for dt="+str(dt))
-    plt.xlabel("t")
-    plt.ylabel("p(t)")
-    handle1,=plt.plot(tip,p,'b',label='Analytic Soln')
-    handle2,=plt.plot(t,p_exp[dt],'r',label=labels)
-    plt.legend(handles=[handle1,handle2])
+    subplot(2, 2, 2)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 10/(1+(9*exp(-tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
 
     ##subplot for dt=1/8
     dt=dt_all[2]
-    plt.subplot(2, 2, 3)
-    t = np.arange(0,tend,dt)
-    tip = np.arange(0,tend,dt_all[-1])
-    p = 10/(1+(9*np.exp(-tip)))
-    plt.title("function p(t) v/s t for dt="+str(dt))
-    plt.xlabel("t")
-    plt.ylabel("p(t)")
-    handle1,=plt.plot(tip,p,'b',label='Analytic Soln')
-    handle2,=plt.plot(t,p_exp[dt],'r',label=labels)
-    plt.legend(handles=[handle1,handle2])
+    subplot(2, 2, 3)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 10/(1+(9*exp(-tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
 
     ##subplot for dt=1/16
     dt=dt_all[3]
-    plt.subplot(2, 2, 4)
-    t = np.arange(0,tend,dt)
-    tip = np.arange(0,tend,dt_all[-1])
-    p = 10/(1+(9*np.exp(-tip)))
-    plt.title("function p(t) v/s t for dt="+str(dt))
-    plt.xlabel("t")
-    plt.ylabel("p(t)")
-    handle1,=plt.plot(tip,p,'b',label='Analytic Soln')
-    handle2,=plt.plot(t,p_exp[dt],'r',label=labels)
-    plt.legend(handles=[handle1,handle2])
+    subplot(2, 2, 4)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 10/(1+(9*exp(-tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
+    plt.show
+    return
+
+def plotter_imp(p_exp,dt_all,tend,labels):
+    import matplotlib.pyplot as plt
+    figure()
+
+
+    ##subplot for dt=1/2
+    dt=dt_all[0]
+    subplot(2, 2, 1)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 200/(20-(10*exp(-7*tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
+
+    ##subplot for dt=1/4
+    dt=dt_all[1]
+    subplot(2, 2, 2)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 200/(20-(10*exp(-7*tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
+
+    ##subplot for dt=1/8
+    dt=dt_all[2]
+    subplot(2, 2, 3)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 200/(20-(10*exp(-7*tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
+
+    ##subplot for dt=1/16
+    dt=dt_all[3]
+    subplot(2, 2, 4)
+    t = arange(0,tend,dt)
+    tip = arange(0,tend,dt_all[-1])
+    p = 200/(20-(10*exp(-7*tip)))
+    title("function p(t) v/s t for dt="+str(dt))
+    xlabel("t")
+    ylabel("p(t)")
+    handle1,=plot(tip,p,'b',label='Analytic Soln')
+    handle2,=plot(t,p_exp[dt],'r',label=labels)
+    legend(handles=[handle1,handle2])
     plt.show()
 
-
+    return
 
 
